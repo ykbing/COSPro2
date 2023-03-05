325866a25e5cd54df2a5cf12c231bb6ac94a5383
ykbing <101696361+ykbing@users.noreply.github.com>
2023-03-05 10:19:19 +0900

    Add files via upload

---
+++ 20230303/34강/example02.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+	int numArr[3][4] = {0,}; 
+	
+	printf("%d \n", numArr[0][0]);
+	printf("%d \n", numArr[1][2]);
+	printf("%d \n", numArr[2][3]);
+	printf("%d \n", numArr[2][0]);	
+}
+ 
